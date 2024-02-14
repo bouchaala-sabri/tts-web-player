41b23c0fb418fc3d55ac1f0bc741b2687128d4d6
Sabri Bouchaala <bouchaala.sabri@gmail.com>
2024-02-14 16:43:40 +0100

    Delete worker.js

--- worker.js
+++
@@ -1,6 +0,0 @@
-self.onmessage = function(event) {
-    const { text, voiceURI } = event.data;
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.voiceURI = voiceURI;
-    speechSynthesis.speak(utterance);
-};
